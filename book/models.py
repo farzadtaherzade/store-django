35631289726570292f4fc451949be5d3ed943197
farzad taherzadeh <farzade.taherzade@gmail.com>
2025-05-16 21:45:22 +0330

    add basket modeld

--- book/models.py
+++ book/models.py
@@ -48,3 +48,37 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.book.name} - {self.rating}"
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="basket")
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def calculate_total_price(self):
+        total = sum(item.get_total_price()
+                    for item in self.items.all().filter(is_ghost=False))
+        self.total_price = total
+        self.save()
+        return
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.name} - {self.quantity}"
+
+
+class BasketItem(models.Model):
+    basket = models.ForeignKey(
+        Basket, on_delete=models.CASCADE, related_name="items")
+    book = models.ForeignKey(Books, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_ghost = models.BooleanField(default=False)
+
+    def get_total_price(self):
+        return self.quantity * self.book.price
+
+    def __str__(self):
+        return f"{self.basket.user.username} - {self.book.name} - {self.quantity}"
