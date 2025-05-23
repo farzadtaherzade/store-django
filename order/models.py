2f8ede82be88483165d21b8f7a93a4fcb10f2571
farzad taherzadeh <farzade.taherzade@gmail.com>
2025-05-23 22:39:47 +0330

    add new model for tracking the payments

--- order/models.py
+++ order/models.py
@@ -53,3 +53,30 @@ class OrderItem(models.Model):
 
     def get_total_price(self):
         return self.quantity * self.price
+
+
+class Payment(models.Model):
+    STATUS_CHOICES = (
+        ("process", "Process"),
+        ("success", "Success"),
+        ("failed", "Failed"),
+    )
+
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="payments"
+    )
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="payments"
+    )
+
+    track_id = models.IntegerField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default="process"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Payment {self.id} for Order {self.order.id}"
